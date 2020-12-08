3752dcadf76fa380a34809888126440ee717b704
sajjathossain <painlesssajjad@gmail.com>
2020-12-09 00:27:04 +0600

    Finished Working (09/12/20)

--- dist/index.js
+++ dist/index.js
@@ -2,31 +2,57 @@
 var addBtn = document.querySelector("#add");
 var task = document.querySelector("#task");
 var showTask = document.querySelector("#add-task");
-var doneBTN = document.querySelector(".doneBTN");
+var doneBTN = document.querySelectorAll("button");
 var deleteBTN = document.querySelector(".deleteBTN");
 var taskList = [];
 function addTask() {
-    if (task.value !== "") {
-        var obj = {
-            taskVal: "",
-            isDone: false
-        };
-        var output_1 = "";
-        obj["taskVal"] = (task.value).toString();
-        obj['isDone'] = false;
-        taskList.push(obj);
-        taskList.forEach(function (el) {
-            output_1 += "\n                <div class=\"col-md-12 col-lg-12 border border-success mt-2 p-2 rounded infos\">\n                    <div class=\"h4 text-warning\">Task " + (taskList.indexOf(el) + 1) + "</div>\n                    <div class=\"h5\">\n                        <div class=\"d-flex flex-row\">\n                            <div class=\"h5 text-secondary\">Task Description :</div>\n                            <div class=\"h5 text-success ml-2\">" + el.taskVal + "</div>\n                        </div>\n                        <span class=\"d-flex flex-row mt-2 float-right\">\n                            <button class=\"btn btn-info mr-2 doneBTN\">Done</button>\n                            <button class=\"btn btn-danger deleteBTN\">Delete</button>\n                        </span>\n                        \n                    </div>\n                </div>\n                ";
-        });
-        showTask.innerHTML = output_1;
+    var obj = {
+        taskVal: "",
+        isDone: ""
+    };
+    obj["taskVal"] = (task.value).toString();
+    obj['isDone'] = "";
+    taskList.push(obj);
+    displayTasks();
+}
+function doneTask(taskId) {
+    taskList[taskId]["isDone"] = "disabled";
+    document.getElementById("" + taskId).style.opacity = '0.5';
+    document.getElementById("" + taskId).style.pointerEvents = 'none';
+    console.log(taskId);
+    console.log(taskList[taskId]["isDone"]);
+}
+function deleteTask(Id) {
+    if (taskList[Id]['isDone'] === "disabled") {
+        taskList.splice(Id, 1);
+        displayTasks();
     }
     else {
-        alert("Please add some text!");
+        var confirmation = prompt("Do you really wanna delete? y/n ");
+        if (confirmation.toLowerCase() === "y") {
+            taskList.splice(Id, 1);
+            displayTasks();
+        }
     }
+    console.log(Id);
+    console.log(taskList);
+}
+function displayTasks() {
+    var output = "";
+    taskList.forEach(function (el) {
+        output += "\n        <div class=\"col-md-12 col-lg-12 border border-success mt-2 p-2 rounded infos\">\n            <div class=\"h4 text-warning\">Task " + (taskList.indexOf(el) + 1) + "</div>\n            <div class=\"descAndButt\">\n                <div class=\"d-flex flex-row\">\n                    <div class=\"h5 text-secondary\">Task Description :</div>\n                    <div class=\"h5 text-success ml-2\">" + el.taskVal + "</div>\n                </div>\n                <div class=\"d-flex flex-row mb-3\">\n                    <button class=\"btn btn-info mr-2 " + el['isDone'] + " \" id=\"" + taskList.indexOf(el) + "\" onClick=\"doneTask(this.id)\">Done</button>\n                    <button class=\"btn btn-danger deleteBTN\" id=\"" + taskList.indexOf(el) + "\" onClick=\"deleteTask(this.id)\">Delete</button>\n                </div>\n                \n            </div>\n        </div>\n        ";
+    });
+    task.value = "";
+    showTask.innerHTML = output;
 }
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Connected perfectly....");
     addBtn.addEventListener("click", function () {
-        addTask();
+        if (task.value !== "") {
+            addTask();
+        }
+        else {
+            alert("Please add some text!");
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,64 +2,85 @@
 const addBtn = document.querySelector("#add")!;
 const task = document.querySelector("#task")! as HTMLInputElement;
 const showTask = document.querySelector("#add-task")!;
-const doneBTN = document.querySelector(".doneBTN")!;
+const doneBTN = document.querySelectorAll("button")!;
 const deleteBTN = document.querySelector(".deleteBTN")!;
-
 
 interface objectTypes {
     taskVal: string,
-    isDone: boolean
+    isDone: string
 }
-
 
 let taskList: Array<objectTypes> = [];
 
-
-
 function addTask() {
-    if (task.value !== "") {
-        
         let obj: objectTypes = {
             taskVal: "",
-            isDone: false
+            isDone: ""
         }
-        let output: string = "";
 
         obj["taskVal"] = (task.value).toString();
-        obj['isDone'] = false;
+        obj['isDone'] = "";
 
 
         taskList.push(obj);
 
-        taskList.forEach((el) => {
-            output += `
-                <div class="col-md-12 col-lg-12 border border-success mt-2 p-2 rounded infos">
-                    <div class="h4 text-warning">Task ${(taskList.indexOf(el) + 1)}</div>
-                    <div class="h5">
-                        <div class="d-flex flex-row">
-                            <div class="h5 text-secondary">Task Description :</div>
-                            <div class="h5 text-success ml-2">${el.taskVal}</div>
-                        </div>
-                        <span class="d-flex flex-row mt-2 float-right">
-                            <button class="btn btn-info mr-2 doneBTN">Done</button>
-                            <button class="btn btn-danger deleteBTN">Delete</button>
-                        </span>
-                        
-                    </div>
-                </div>
-                `;
-            
-        });
+        displayTasks();        
         
-        showTask.innerHTML = output;
-        
-    }else {
-        alert("Please add some text!");
-    }
         
 }
 
 
+function doneTask(taskId: number) {
+    taskList[taskId]["isDone"] = "disabled";
+    document.getElementById(`${taskId}`)!.style.opacity = '0.5';
+    document.getElementById(`${taskId}`)!.style.pointerEvents = 'none';
+    console.log(taskId);
+    console.log(taskList[taskId]["isDone"]);
+}
+
+
+function deleteTask(Id: number) {
+    if (taskList[Id]['isDone'] === "disabled") {
+        taskList.splice(Id, 1);
+        displayTasks();
+    } else {
+        const confirmation = prompt("Do you really wanna delete? y/n ")!;
+        if (confirmation.toLowerCase() === "y") {
+            taskList.splice(Id, 1);
+            displayTasks();
+        }
+    }
+    console.log(Id);
+    console.log(taskList);
+}
+
+function displayTasks() {
+    let output: string = "";
+    taskList.forEach((el) => {
+        
+        output += `
+        <div class="col-md-12 col-lg-12 border border-success mt-2 p-2 rounded infos">
+            <div class="h4 text-warning">Task ${(taskList.indexOf(el) + 1)}</div>
+            <div class="descAndButt">
+                <div class="d-flex flex-row">
+                    <div class="h5 text-secondary">Task Description :</div>
+                    <div class="h5 text-success ml-2">${el.taskVal}</div>
+                </div>
+                <div class="d-flex flex-row mb-3">
+                    <button class="btn btn-info mr-2 ${el['isDone']} " id="${taskList.indexOf(el)}" onClick="doneTask(this.id)">Done</button>
+                    <button class="btn btn-danger deleteBTN" id="${taskList.indexOf(el)}" onClick="deleteTask(this.id)">Delete</button>
+                </div>
+                
+            </div>
+        </div>
+        `;
+         
+    });
+
+    task.value = "";
+    showTask.innerHTML = output;
+        
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -68,8 +89,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     addBtn.addEventListener("click", () => {
 
-        addTask();
-
+        if (task.value !== "") {
+            addTask();
+        } else {
+            alert("Please add some text!");
+        }
     });
 
 });
